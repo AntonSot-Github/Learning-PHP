8308c716e18453137e85f7818c4580af656574fb
Anton <777elisar777@gmail.com>
2024-08-15 16:40:17 +0200

    switch and match

--- switch_case.php
+++ switch_case.php
@@ -1,6 +1,8 @@
 <?php
 //Операторы SWITCH/CASE для сравнения переменной с чем либо 
+require_once "function_n.php";
 
+nStr("Switch/case");
 $role = "user";
 
 switch ($role){
@@ -16,3 +18,29 @@ switch ($role){
     default:
         echo "role is underfined", "\n";
 }
+
+$t = rand(0, 16);
+switch ($t){
+    case 1:
+    case 2:
+    case 3:
+        echo "variable = 1 - 3", "\n";//Действие для первых трех проверок выполняется одно и то же
+        break;
+    case ($t >= 4 && $t <= 10):
+        echo $t, "\n";
+        break;
+    default:
+        echo "variable more than 10", "\n";
+}
+
+nStr("Match");
+$r = rand(0, 5);
+echo match ($r) {
+    0 => "zero",
+    1 => "one",
+    2 => "two",
+    3 => "three",
+    4 => "four",
+    5 => "maximum value",
+    default => "invalid value",
+};
--- Тернарный оператор.php
+++ Тернарный оператор.php
@@ -6,3 +6,16 @@ $res;
 
 $res = ($resault === 5) ? $res = 10 : $res = 15;
 echo $res, "\n";
+
+//another one example
+$t = rand(0, 1);//генерирует случайное число от 0 до 1
+echo $t ? 'true' : 'false', "\n";
+
+$r = rand(0, 10);
+echo $r, "\n";
+echo ($r % 2 == 0) ?: "false", "\n";//возвращает 1, ксли условие - true
+
+$u = 5;
+echo $u ?: "false", "\n";//вернёт значение переменной - 5
+echo is_int($u)?: "false", "\n";//вернёт 1
+echo $g?? "doesn't exist", "\n";//doesn't exist
