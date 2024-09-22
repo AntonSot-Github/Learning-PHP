9e55ada6345899621975b488b33de8f37f638d49
Anton <777elisar777@gmail.com>
2024-09-22 16:43:49 +0200

    Home Task version 2

--- Counter of visits.php
+++ Counter of visits.php
@@ -10,6 +10,33 @@ setcookie('QuentetyOfEnters', $count);
 
 ?>
 
+<?php
+//   Teacher's version 
+
+//проверяем, существует ли кука
+if (isset($_COOKIE['teacherCount'])) {
+    //если существует
+    setcookie('teacherCount', ++$_COOKIE['teacherCount'], time() + 3600, '/'); //увеличиваем значение на 1(префиксный инкримент), устанавливаем время жизни куки(1час), распространяем доступ на весь домен
+} else {
+    //если не существует, создаем куку и присваиваем значение 1
+    setcookie('teacherCount', 1, time() + 3600, '/');
+}
+
+//То же самое условие с тернарным оператором
+//isset($_COOKIE['teacherCount'])) ? setcookie('teacherCount', ++$_COOKIE['teacherCount'], time() + 3600, '/') : setcookie('teacherCount', 1, time() + 3600, '/');
+
+/* Обнуляем счетчик c помощью GET-запроса */
+if (isset($_GET['do']) && $_GET['do'] == 'reset'){
+    //Удаляем куку
+    setcookie('teacherCount', "", time() - 3600, '/');
+    //Перенаправляем на ту же страницу
+    header('Location: Counter of visits.php');
+    die;
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,6 +64,13 @@ setcookie('QuentetyOfEnters', $count);
         <h1>Counter of visits</h1>
         <p>Quentity of visits: <?php echo($_COOKIE['QuentetyOfEnters']) ?></p>
         <a href="Help_files_PHP/setcookie.php">Setcookie</a>
+    </div>
+
+    <!-- Teacher's version -->
+
+    <div class="wrapper">
+    <p>Quentity of visits: <?php echo($_COOKIE['teacherCount'] ?? 1) ?></p>
+    <p><a href="?do=reset">Reset counter</a></p><!-- Создаем GET-запрос для удаления куки -->
     </div>
 </body>
 </html>
