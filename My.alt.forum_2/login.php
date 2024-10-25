3d581e49dc50f90d40be6b888b16feffb64b560c
Anton <777elisar777@gmail.com>
2024-10-25 19:09:36 +0200

    New forum started

---
+++ My.alt.forum_2/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alternative forum 2</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="nav">
+        <div class="nav__header-box">
+            <h1>Alternative forum - Version 2</h1>
+        </div>
+        <div class="nav__box">
+            <a href="#" class="nav__link nav__themes">Themes for conversations</a>
+            <a href="#" class="nav__link nav__sites">Choose the site</a>
+            <a href="#" class="nav__link nav__user">User</a>
+        </div>
+    </div>
+    <div class="container">
+
+        <div class="form-registration">
+
+            <form method="post">
+                <input class="form-input form-input__tel" type="tel" name="tel" placeholder="Your telephon number">
+                <input class="form-input form-input__email" type="email" name="email" placeholder="Your email">
+                <button class="btn" type="submit">Registration</button>
+            </form>
+        </div>
+    </div>
+    <footer>
+        <h2>WELCOME!</h2>
+    </footer>
+</body>
+</html>
