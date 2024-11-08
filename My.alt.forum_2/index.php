75502be04f9762059ac1a26e4c4db82a2ac56e8d
Anton <777elisar777@gmail.com>
2024-11-08 12:42:15 +0100

    little changes

--- My.alt.forum_2/index.php
+++ My.alt.forum_2/index.php
@@ -18,13 +18,22 @@
             <?php echo ((isset($_GET['do']) && $_GET['do'] == 'createTopic') ? 'no-display' : 'display')?>">
 
                 <p>There is no any topics yet</p>
-                <a class="link btn btn-grad btn-msg" href="?do=createTopic">Create new topic for conversation</a>
+                <a class="link btn btn-grad btn-msg" href="?do=createTopic">Create a new topic</a>
             </div>
         <?php endif; ?>
 
         <div class="topic <?php echo ((isset($_GET['do']) && $_GET['do'] == 'createTopic') ? 'display' : 'no-display')?>">
 
-            <p>There is no any topics yet</p>
+            <?php if(isset($_SESSION['user'])): ?>
+                <form>
+                    
+                </form>
+
+            <?php else: ?>
+                    <p>Only autorized users can create new topics. Please log in to our website
+                        <em><a class="link link-underlining_left-to-right link__reg" href="login.php">Login</a></em>
+                    </p>
+            <?php endif; ?>
 
         </div>
 
